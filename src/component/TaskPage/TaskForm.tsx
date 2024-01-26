51b7370369842b0af7539fae4338c77ddf14023f
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2024-01-26 15:55:58 -0500

    date

--- src/component/TaskPage/TaskForm.tsx
+++ src/component/TaskPage/TaskForm.tsx
@@ -103,7 +103,7 @@ const TaskForm = ({ onSubmition, onClick }: Props) => {
           value={date}
           name="dueDate"
           onChange={(e) => setDate(e.target.value)}
-          className="block h-10 min-w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
           required
         />
       </div>
